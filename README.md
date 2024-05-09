bda88cfa67837d4f15c83c07c8356f8754afa91f
YangH9 <49155616+YangH9@users.noreply.github.com>
2024-05-09 16:50:12 +0800

    提交

---
+++ README.md
@@ -0,0 +1,40 @@
+# ip-white-proxy-server
+
+前端开发环境 ip 白名单代理服务
+
+## 安装依赖
+
+```
+npm install
+```
+
+## 修改配置
+
+```
+config.json
+
+{
+  "port": 3000, // 代理端口，访问的目标端口
+  "ipList": ['127.0.0.1', '::1'] // ip白名单，允许访问的ip列表
+  "proxyTarget": "http://localhost:8080" // 代理url，本地项目的url
+}
+```
+
+## 启动服务
+
+```
+npm start
+```
+
+## 快捷启动
+
+> 创建一个 bat 文件，写入下列命令
+
+```
+D:
+cd "D:\ip-white-proxy-server"
+cmd /c npm start
+```
+
+> D: // 切换到项目所在盘符
+> cd "D:\ip-white-proxy-server" // 切换到项目所在目录
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const { port, ipList, proxyTarget } = require('./config')
+
+const app = express()
+
+const stripIPv4MappedIPv6 = address => (address.startsWith('::ffff:') ? address.substring(7) : address)
+
+app.use((req, res, next) => {
+  const ip = stripIPv4MappedIPv6(req.ip)
+  if (ipList.find(i => i === ip)) {
+    // res.status(200).send(`<h1>Hello World</h1>`)
+    next()
+  } else {
+    res.status(403).send(`<h1>IP地址${req.ip}不在白名单中，访问被拒绝。</h1>`)
+  }
+})
+
+app.use(
+  createProxyMiddleware({
+    target: proxyTarget,
+    changeOrigin: true
+  })
+)
+
+// 监听端口
+app.listen(port, err => {
+  if (err) {
+    console.log(`Express服务启动失败，目标端口：${port}`)
+  } else {
+    console.log(`Express服务已启动，监听端口： ${port}`)
+  }
+})
